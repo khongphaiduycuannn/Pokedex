6931f0f186e8c927ce1a3f5594bb072414c18df6
Duy Cuan <ndmquan1010@gmail.com>
2024-06-24 15:18:19 +0700

    fake sample data

---
+++ app/src/main/java/com/example/androidmvvmprojectbase/data/source/SampleData.kt
@@ -0,0 +1,164 @@
+package com.example.androidmvvmprojectbase.data.source
+
+import com.example.androidmvvmprojectbase.data.Pokemon
+import com.example.androidmvvmprojectbase.data.Status
+
+object SampleData {
+
+    val pokemonList = mutableListOf(
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        ),
+        Pokemon(
+            id = "1",
+            name = "Bulbasaur",
+            image = "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+            description = "Bulbasaur is a small, mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, with the bulb resembling a plant bulb that grows into a flower as it evolves.",
+            type = listOf(
+                "Grass",
+                "Poison"
+            ),
+            height = 0.7,
+            weight = 6.9,
+            nextEvolution = "2",
+            prevEvolution = "0",
+            status = Status(
+                hp = 45,
+                attack = 49,
+                defense = 49,
+                spAtk = 65,
+                spDef = 65,
+                speed = 45
+            )
+        )
+    )
+}
